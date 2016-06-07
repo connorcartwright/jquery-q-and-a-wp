d426ad08ad8c87be927320384e73421e4ab64139
Connor Cartwright <connorcart@gmail.com>
2016-06-07 18:10:59 +0100

    first commit

---
+++ jquery-q-and-a.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: jQuery Q & A
+Plugin URI: http://www.jquery.com
+Description: Plugin to insert interactive question/answer content.
+Author: Connor Cartwright
+Version: 0.1
+Author URI: http://www.jquery.org
+*/
+
+add_action('admin_menu', 'add_option_page');
+add_action( 'admin_enqueue_scripts', 'my_enqueue' );
+add_action( 'wp_ajax_embed_question', 'embed_question' );
+// add_action( 'wp_ajax_preview_question', 'preview_question' );
+add_action( 'wp_ajax_update_page', 'update_page' );
+
+function add_option_page(){
+    enqueue_styles();
+    add_menu_page( 'jQuery Question and Answer', 'jQuery Q & A', 'manage_options', 'jquery-q-and-a', 'init_options_page', 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Question_mark_white_icon.svg');
+}
+
+function enqueue_styles() {
+    wp_register_style( 'plugin_style', plugins_url( 'css/style.css' , __FILE__ ) );
+    wp_enqueue_style( 'plugin_style' );
+
+    wp_register_style( 'glyphicons', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css');
+    wp_enqueue_style( 'glyphicons' );
+}
+
+function enqueue_scripts($pages) {
+    wp_register_script( 'jQuery', 'https://code.jquery.com/jquery-2.2.4.min.js' );
+    wp_enqueue_script( 'jQuery' );
+
+    wp_register_script( 'q&a', plugins_url('js/q&a.js',__FILE__));
+    wp_enqueue_script( 'q&a' );
+
+    wp_localize_script( 'q&a', 'wp_ajax', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) ); // add wp ajax object into script
+    wp_localize_script( 'q&a', 'wp_pages', $pages ); // pass page data into script
+}
+
+function init_options_page(){
+    ?>
+    <div class="wrap" id="q-and-a-plugin">
+            <?php
+                $pages = array();
+                $page_ids=get_all_page_ids();
+                foreach($page_ids as $id)
+                {
+                    array_push( $pages, array($id, get_the_title($id), get_page_link($id) ) );
+                }
+                enqueue_scripts($pages);
+            ?>
+
+    </div>
+
+    <?php
+}
+
+function embed_question() {
+    $page_id = intval( $_POST['page_id'] );
+    $page = get_post( $page_id, OBJECT, 'edit' ); // get post
+    $content = $page->post_content; // get content
+    $settings =   array(
+        'media_buttons' => false, // hide media buttons
+    );
+
+    wp_editor( $content, 'embed question', $settings ); // create editor
+    wp_die(); // this is required to terminate immediately and return a proper response
+}
+
+function update_page() {
+    $post_id = intval( $_POST['post_id'] );
+
+    $updated_post = array(
+        'ID'           =>  $post_id,
+        'post_title'   => get_the_title($post_id),
+        'post_content' => $_POST['post_content'],
+    );
+
+    wp_update_post( $updated_post ); // Update the post into the database
+    wp_die(); // this is required to terminate immediately and return a proper response
+}
+
+
+?>
---
+++ js/q&a.js
@@ -0,0 +1,206 @@
+$(function() {
+
+    setup();
+    addModals();
+
+    function setup() {
+
+        var $plugin_body = $( '<div class="plugin-body"></div>' );
+        $plugin_body.append( '<h1>Learning Center Pages</h1>' );
+
+        var $page_table = $('<div class="qa-tbl"></div>');
+
+        var $table_header = $( '<div class="qa-tbl-row qa-tbl-hdr qa-stripe"></div>' );
+        $table_header.append( '<div class="qa-page-id qa-center"><span>Page ID</span></div>' );
+        $table_header.append( '<div class="qa-page-title"><span>Page Title</span></div>' );
+        $table_header.append( '<div class="qa-page-q-count qa-center"><span>Question Count</span></div>' );
+        $table_header.append( '<div class="qa-page-preview qa-center"><span>Preview</span></div>' );
+
+        $page_table.append( $table_header );
+
+        $page_table.append( getPageRows() );
+
+        $plugin_body.append( $page_table );
+        $( 'div#q-and-a-plugin' ).append( $plugin_body );
+    }
+
+    function getPageRows() {
+        var $pageRows = $();
+        for (var i = 0; i < wp_pages.length; i++) {
+            var page = wp_pages[i];
+            var $row = $( '<div class="qa-tbl-row qa-stripe page" data-p-id="' + page[0] + '" data-p-title="' + page[1] + '"></div>' );
+            $row.append( '<div class="qa-page-id qa-center"><span>' + page[0] + '</span></div>' );
+            $row.append( '<div class="qa-page-title"><span>' + page[1] + '</span></div>' );
+            $row.append( '<div class="qa-page-q-count qa-center"><span> 0 </span></div>' );
+            $row.append( '<div class="qa-page-preview qa-center"><button type="button" class="btn btn-success page-preview"><span class="glyphicon glyphicon-eye-open"></span><a href="' + page[2] + '" target="_blank" rel="noopener"></a></button></div>' );
+            $pageRows = $pageRows.add($row);
+        }
+        return $pageRows;
+    }
+
+    function addModals() {
+        $('div#q-and-a-plugin').append('<div id ="q-embed-preview-modal" class="modal"><div class="modal-content"><div class="modal-close"><span class="glyphicon glyphicon-remove"></span></div><div class="modal-header"><h1></h1></div><div class="modal-body"></div><div class="modal-footer"><div class="modal-footer-buttons"><button class="btn btn-default">Close</button><button class="btn btn-success">Save</button></div></div></div></div>')
+    }
+
+    // stop click of the preview opening/closing a row
+    $('button.page-preview').click(function(e) {
+        e.stopPropagation();
+    });
+
+    $('div.qa-tbl-row.page').on('click', function() {
+        var page_id = $(this).data('p-id');
+        var page_title = $(this).data('p-title');
+
+        // if the questions for this page are open
+        if ($(this).next().hasClass('questions')) {
+            $('div.questions, div.blank-row').slideUp(400, function() {
+                $(this).remove();
+            });
+        } else {
+
+            $('div.questions, div.blank-row').remove();
+
+            var questions = $('<div class="questions"></div></div>')
+            var questionTable = $('<div class="qst-table"></div>');
+            questionTable.append('<div class="qa-tbl-row qa-tbl-hdr"><div class="q-name"><span>Question Name</span></div><div class="q-embed qa-center"><span>Embed</span></div>' +
+                '<div class="q-edit qa-center"><span>Edit</span></div><div class="q-preview qa-center"><span>Preview</span></div><div class="q-report qa-center"><span>View Report</span></div>' +
+                '<div class="q-delete qa-center"><span>Delete</span></div></div>');
+
+            for (var i = 0; i < 5; i++) {
+                var question = $('<div class="qa-tbl-row question"></div>');
+                question.append('<div class="q-name"><span>How to use $(#my-element).after</span></div>');
+
+                var embedButton = $('<div class="q-embed qa-center"><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-link"></span></button></td>');
+
+                embedButton.find('button').on('click', function() {
+                    questionEmbedButtonClick(page_id, page_title);
+                });
+
+                var editButton = $('<div class="q-edit qa-center"><button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></span></button></div>');
+
+                editButton.find('button').on('click', function() {
+                   questionEditButtonClick(); // and question_id
+                });
+
+                var previewButton = $('<div class="q-preview qa-center"><button type="button" class="btn btn-success"><span class="glyphicon glyphicon-eye-open"></span></button><div/>');
+
+                previewButton.find('button').on('click', function() {
+                   // questionPreviewButtonClick(page_id, page_title);
+                });
+
+                var reportButton = $('<div class="q-report qa-center"><button type="button" class="btn btn-info"><span class="glyphicon glyphicon-list-alt"></span></button></div>');
+
+                reportButton.find('button').on('click', function() {
+                    questionReportButtonClick(); // question id
+                });
+
+                var deleteButton = $('<div  class="q-delete qa-center"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button></div>');
+
+                deleteButton.find('button').on('click', function() {
+                    questionDeleteButtonClick() // question_id
+                });
+
+                question.append(embedButton);
+                question.append(editButton);
+                question.append(previewButton);
+                question.append(reportButton);
+                question.append(deleteButton);
+                questionTable.append(question);
+            }
+
+            questionTable.append('<div class="qa-tbl-row question"><div class="q-add"><button type="button" class="btn btn-success"><span class="glyphicon glyphicon-plus"></span></button></div>');
+            var blankSpace = $('<div class="qa-tbl-row blank-row"></div>');
+            $(this).after(questions);
+            questions.append(questionTable).after(blankSpace);
+            blankSpace.height(questions.height());
+            questions.add(blankSpace).hide().slideDown(600);
+        }
+    });
+
+
+
+    $('div.modal-close').add('div.modal-footer .btn-default').on('click', function() {
+       $('div.modal').fadeOut(300);
+    });
+
+    function questionEmbedButtonClick(page_id, page_title) {
+        var data = {
+            action: 'embed_question',
+            page_id: page_id
+        };
+
+        $.ajax({
+            type: "post",
+            dataType: "json",
+            url: wp_ajax.ajax_url,
+            data: data
+        })
+        .done(function(data) {
+            console.log( "success - get edit page" );
+        })
+        .fail(function() {
+            console.log( "error - get edit page" );
+        })
+        .always(function(data) {
+            console.log('always - get edit page');
+            console.log(data);
+
+            $('div#q-embed-preview-modal').data('p-id', page_id);
+            $('div#q-embed-preview-modal').data('p-title', page_title);
+            $('div#q-embed-preview-modal').fadeIn(600);
+            $('div#q-embed-preview-modal div.modal-header>h1').text(page_title + ': <iframe src="http://www.example.com"></iframe>');
+            $('div#q-embed-preview-modal div.modal-body').empty().append(data.responseText);
+        });
+    }
+
+    function questionEditButtonClick() { // question id
+        // TO DO
+    }
+
+    // function questionPreviewButtonClick() {
+    //     // TO DO - probably just an <a> link
+    // }
+
+    function questionReportButtonClick() { // question id
+        // TO DO
+    }
+
+    function questionDeleteButtonClick() { // question id
+        // TO DO
+    }
+
+    $('div#q-embed-preview-modal button.btn-success').on('click', function() {
+        var page_id = $('div#q-embed-preview-modal').data('p-id');
+        var page_title = $('div#q-embed-preview-modal').data('p-title');
+        var page_content = $('div#q-embed-preview-modal textarea.wp-editor-area').val();
+
+        var data = {
+            action: 'update_page',
+            page_id: page_id,
+            page_content: page_content
+        };
+
+        $.ajax({
+            type: "post",
+            dataType: "json",
+            url: wp_ajax.ajax_url,
+            data: data
+        })
+        .done(function(data) {
+            console.log( "success - update page" );
+        })
+        .fail(function() {
+            console.log( "error - update page" );
+        })
+        .always(function(data) {
+            console.log('always - update page');
+            console.log(data);
+            var $message = $('<div class="modal-message-overlay"><div class="modal-message-content"><h2>The ' + page_title +  ' page has been updated successfully!</h2></div></div>');
+
+            $('div#q-embed-preview-modal .modal-content').append($message);
+            $message.hide().fadeIn(1000, function() { setTimeout(function() { $message.fadeOut(800, function() { $message.remove(); }) }, 1200) });
+        });
+    });
+
+
+});
