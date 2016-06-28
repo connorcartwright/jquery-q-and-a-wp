eed677abc4de6b6b86e00c6b257a8985c7372c5f
Connor Cartwright <connorcart@gmail.com>
2016-06-28 16:21:36 +0100

    Camel cased question-modal.js

--- js/modals/question-modal.js
+++ js/modals/question-modal.js
@@ -37,37 +37,43 @@ $(function() {
                 .removeClass('edit-question')
                 .text('Create');
 
-        var modal_body = modal.find('.modal-body');
-        modal_body.empty();
+        var modalBody = modal.find('.modal-body');
+        modalBody.empty();
 
-        var question_form = $('<form></form>');
-        var question_name = $('<fieldset class="form-group"> <label for="q-name-input">Question Name </label> <input type="text" id="q-name-input" class="form-control" placeholder="Question Name"></fieldset>');
-        var question_type = $('<fieldset class="form-group"> <label for="q-type-select">Question Type </label></fieldset>');
-        var question_statement = $('<fieldset class="form-group"> <label for="q-statement-input">Question Statement</label><textarea class="form-control" id="q-statement-input" rows="3" placeholder="Question Statement"></textarea></fieldset>');
+        var questionForm = $('<form></form>');
+        var questionName = $('<fieldset class="form-group"> <label for="q-name-input">Question Name </label> <input type="text" id="q-name-input" class="form-control" placeholder="Question Name"></fieldset>');
+        var questionType = $('<fieldset class="form-group"> <label for="q-type-select">Question Type </label></fieldset>');
+        var questionStatement = $('<fieldset class="form-group"> <label for="q-statement-input">Question Statement</label><textarea class="form-control" id="q-statement-input" rows="3" placeholder="Question Statement"></textarea></fieldset>');
 
-        var type_select = $('<select class="form-control" id="q-type-select"><option>Multiple Choice</option><option>Bug Fix</option><option>Missing Code</option><option>Complete Code</option></select>');
-        var question_type_area = $('<div class="question-type-area multiple-choice"></div>');
-        question_type_area.html(questionTypeChange(type_select.find('option:selected').text()));
+        var typeSelect = $('<select class="form-control" id="q-type-select"><option>Multiple Choice</option><option>Bug Fix</option><option>Missing Code</option><option>Complete Code</option></select>');
+        var questionTypeArea = $('<div class="question-type-area multiple-choice"></div>');
+        questionTypeArea.html(questionTypeChange(typeSelect.find('option:selected').text()));
 
-        $(type_select).on('change', function () {
-            var type = type_select.find('option:selected').text();
-            question_type_area.html(questionTypeChange(type));
-            if (type != 'Multiple Choice') {
-                var editor = ace.edit('editor');
-                editor.setTheme('ace/theme/monokai');
-                editor.getSession().setMode("ace/mode/javascript");
-            }
+        $(typeSelect).on('change', function () {
+            var type = typeSelect.find('option:selected').text();
+
+                // if type wasn't already MC, AND the area isn't already coding
+            console.log($('.question-type-area.coding').length);
+
+                if (type == 'Multiple Choice') {
+                    questionTypeArea.html(questionTypeChange(type));
+                } else if (!$('.question-type-area.coding').length) {
+                    questionTypeArea.html(questionTypeChange(type));
+                    var editor = ace.edit('editor');
+                    editor.setTheme('ace/theme/monokai');
+                    editor.getSession().setMode("ace/mode/javascript");
+                }
         });
 
-        question_type.append(type_select);
-        question_form
-            .append(question_name)
-            .append(question_type)
-            .append(question_statement)
-            .append(question_type_area)
+        questionType.append(typeSelect);
+        questionForm
+            .append(questionName)
+            .append(questionType)
+            .append(questionStatement)
+            .append(questionTypeArea)
             .append(createHints());
 
-        modal_body.append(question_form);
+        modalBody.append(questionForm);
 
         modal.fadeIn(600);
         $('body').css('overflow', 'hidden');
@@ -76,18 +82,18 @@ $(function() {
 
     function createHints() {
         var $hints = $('<fieldset class="form-group hints"> <label>Hints </label></fieldset>');
-        var hint_button_1 = '<button type="button" class="btn btn-default hint hint-1 active" data-hint="1"><span>Hint 1</span></button>';
-        var hint_button_2 = '<button type="button" class="btn btn-default hint hint-2" data-hint="2"><span>2</span></button>';
-        var hint_button_3 = '<button type="button" class="btn btn-default hint hint-3" data-hint="3"><span>3</span></button>';
-        var $hint_textarea = $('<textarea class="form-control" id="q-statement-input" rows="3" placeholder="Hint 1"></textarea>');
+        var hintButton1 = '<button type="button" class="btn btn-default hint hint-1 active" data-hint="1"><span>Hint 1</span></button>';
+        var hintButton2 = '<button type="button" class="btn btn-default hint hint-2" data-hint="2"><span>2</span></button>';
+        var hintButton3 = '<button type="button" class="btn btn-default hint hint-3" data-hint="3"><span>3</span></button>';
+        var $hintTextarea = $('<textarea class="form-control" id="q-statement-input" rows="3" placeholder="Hint 1"></textarea>');
 
         $hints
-            .append(hint_button_1)
-            .append(hint_button_2)
-            .append(hint_button_3)
-            .append($hint_textarea);
+            .append(hintButton1)
+            .append(hintButton2)
+            .append(hintButton3)
+            .append($hintTextarea);
 
-        $hint_textarea.on('keyup', function () {
+        $hintTextarea.on('keyup', function () {
             $('.hint.active').data('hint-text', $(this).val());
         });
 
@@ -124,10 +130,10 @@ $(function() {
     function createMultipleChoiceArea() {
         $('.question-type-area').attr('class', 'question-type-area multiple-choice');
 
-        var add_remove_options = $('<fieldset class="form-group mc-option-change">' +
+        var addRemoveOptions = $('<fieldset class="form-group mc-option-change">' +
             '<button type="button" class="btn btn-default mc-remove-option"><span class="glyphicon glyphicon-minus"></span></button>' +
             '<button type="button" class="btn btn-default mc-add-option"><span class="glyphicon glyphicon-plus"></span></button></fieldset>');
-        return add_remove_options.add(createMultipleChoiceOptions(4));
+        return addRemoveOptions.add(createMultipleChoiceOptions(4));
     }
 
     bindMultipleChoiceEvents();
@@ -167,9 +173,9 @@ $(function() {
             var option = $('<fieldset class="form-group mc-text-option"><div class="input-group input"><input type="text" class="form-control wrong" id="q-mc-option-' + i + '" placeholder="Option ' + ((i + 1) + $('.mc-text-option').length) + ' Text">' +
                 '<div class="input-group-addon correct-answer"></div></div></div></fieldset>');
 
-            var correct_button = $('<button type="button" class="btn btn-default mc-correct-answer"><span class="glyphicon glyphicon-ok"></span></button>');
+            var correctButton = $('<button type="button" class="btn btn-default mc-correct-answer"><span class="glyphicon glyphicon-ok"></span></button>');
 
-            option.find('.correct-answer').append(correct_button);
+            option.find('.correct-answer').append(correctButton);
 
             r = r.add(option);
         }
@@ -178,21 +184,22 @@ $(function() {
     }
 
     function createCodeArea() {
+        $('.question-type-area').attr('class', 'question-type-area coding');
         var editor = $('<div id="editor" class="code-editor mc-code">// Enter your code here</div>');
         var io = $('<fieldset class="form-group input-output"> <label>Input / Output </label></fieldset>');
-        var remove_io = $('<button type="button" class="btn btn-default remove-io"><span class="glyphicon glyphicon-minus"></span></button>');
-        var add_io = $('<button type="button" class="btn btn-default add-io"><span class="glyphicon glyphicon-plus"></span></button>');
-        var io_1 = $('<button type="button" class="btn btn-default io active" data-io="1"><span>IO 1</span></button>');
-        var io_2 = $('<button type="button" class="btn btn-default io" data-io="2"><span>2</span></button>');
-        var io_3 = $('<button type="button" class="btn btn-default io" data-io="3"><span>3</span></button>');
+        var removeIo = $('<button type="button" class="btn btn-default remove-io"><span class="glyphicon glyphicon-minus"></span></button>');
+        var addIo = $('<button type="button" class="btn btn-default add-io"><span class="glyphicon glyphicon-plus"></span></button>');
+        var io1 = $('<button type="button" class="btn btn-default io active" data-io="1"><span>IO 1</span></button>');
+        var io2 = $('<button type="button" class="btn btn-default io" data-io="2"><span>2</span></button>');
+        var io3 = $('<button type="button" class="btn btn-default io" data-io="3"><span>3</span></button>');
 
-        remove_io.on('click', function () {
+        removeIo.on('click', function () {
             if ($('.io').length > 3) {
                 $('.io').last().remove();
             }
         });
 
-        add_io.on('click', function () {
+        addIo.on('click', function () {
             var io_length = $('.io').length;
             if (io_length < 8) {
                 var io_count = ($('.io').length + 1);
@@ -201,26 +208,26 @@ $(function() {
             }
         });
 
-        var input_textarea = $('<textarea class="form-control" id="input_textarea" rows="3" placeholder="Question Input 1"></textarea>');
-        var output_input = $('<input type="text" id="output-input" class="form-control" placeholder="Expected Output 1">');
+        var inputTextarea = $('<textarea class="form-control" id="input_textarea" rows="3" placeholder="Question Input 1"></textarea>');
+        var outputInput = $('<input type="text" id="output-input" class="form-control" placeholder="Expected Output 1">');
 
-        io.append(remove_io);
-        io.append(add_io);
-        io.append(io_1);
-        io.append(io_2);
-        io.append(io_3);
-        io.append(input_textarea);
-        io.append(output_input);
+        io.append(removeIo);
+        io.append(addIo);
+        io.append(io1);
+        io.append(io2);
+        io.append(io3);
+        io.append(inputTextarea);
+        io.append(outputInput);
 
         $('.modal').on('click', '.io', function () {
             ioButtonClick($(this));
         });
 
-        input_textarea.on('keyup', function () {
+        inputTextarea.on('keyup', function () {
             $('.io.active').data('input', $(this).val());
         });
 
-        output_input.on('keyup', function () {
+        outputInput.on('keyup', function () {
             $('.io.active').data('output', $(this).val());
         });
 
@@ -246,23 +253,23 @@ $(function() {
     $('.modal').on('click', ' .create-question', function () {
         $('.modal *').removeClass('error');
         $('.modal .error-text').remove();
-        var q_name = $('#q-name-input');
+        var questionName = $('#q-name-input');
 
-        if (q_name.val().length < 10) {
-            q_name.addClass('error');
+        if (questionName.val().length < 10) {
+            questionName.addClass('error');
         }
 
-        var q_statement = $('#q-statement-input');
+        var questionStatement = $('#q-statement-input');
 
-        if (q_statement.val().length < 50) {
-            q_statement.addClass('error');
+        if (questionStatement.val().length < 50) {
+            questionStatement.addClass('error');
         }
 
-        var q_type = $('#q-type-select').find('option:selected').text();
+        var questionType = $('#q-type-select').find('option:selected').text();
 
-        var q_code;
+        var questionCode;
 
-        if (q_type == 'Multiple Choice') {
+        if (questionType == 'Multiple Choice') {
             var option_count = $('.mc-text-option').length;
             if (!$('.mc-text-option .form-control.wrong').length) {
                 $('.mc-option-change').after('<span class="error-text">There must be at least one wrong answer!</span>');
@@ -272,7 +279,7 @@ $(function() {
             }
         }
         else {
-            q_code = ace.edit('editor').getValue();
+            questionCode = ace.edit('editor').getValue();
 
             $('.io').each(function () {
                 if (!$(this).data('input') || !$(this).data('output')) {
@@ -292,10 +299,10 @@ $(function() {
             }
         });
 
-        console.log('q_name: ' + q_name);
-        console.log('q_type: ' + q_type);
-        console.log('q_statement: ' + q_statement);
-        console.log('q_code: ' + q_code);
+        console.log('question name: ' + questionName);
+        console.log('question type: ' + questionType);
+        console.log('question statement: ' + questionStatement);
+        console.log('question code: ' + questionCode);
         console.log('success');
     });
 
