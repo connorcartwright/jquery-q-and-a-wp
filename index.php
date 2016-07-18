c3eec8cee1c3cffabc3f03db350c8d9c467fb8a3
Connor Cartwright <connorcart@gmail.com>
2016-07-18 15:00:22 +0100

    Added in redirects to homepage, working now

--- index.php
+++ index.php
@@ -30,10 +30,21 @@ function enqueue_styles() {
     wp_enqueue_style( 'glyphicons' );
 }
 
+function enqueue_scripts($pages) {
+    wp_register_script( 'jQuery', 'https://code.jquery.com/jquery-2.2.4.min.js' );
+    wp_enqueue_script( 'jQuery' );
+
+    wp_register_script( 'q&a', plugins_url('jquery-q-and-a.js',__FILE__));
+    wp_enqueue_script( 'q&a' );
+
+    wp_localize_script( 'q&a', 'wpAjax', array( 'ajaxUrl' => admin_url( 'admin-ajax.php' ) ) ); // add wp ajax object into script
+    wp_localize_script( 'q&a', 'pages', $pages ); // pass page data into script
+
+    wp_register_script( 'ace', 'https://cdn.jsdelivr.net/ace/1.2.3/min/ace.js' );
+    wp_enqueue_script( 'ace' );
+}
+
 function init_options_page(){
-    ?>
-    <div class="wrap q-and-a-plugin" id="q-and-a-plugin"><h1>jQuery Q&A</h1></div>
-    <?php
     main();
 }
 
@@ -41,60 +52,46 @@ function main() {
     global $wpdb;
     $plugin = new \QA\Plugin($wpdb, $wpdb->prefix . 'qaUsers6', 3);
     $plugin->createUserTable();
-
+    $plugin->addCurrentUser();
     $token = $plugin->getAccessToken()->access_token;
 
     if (!$token) {
         if(isset($_GET["access-token"])) {
             $urlToken = $_GET["access-token"];
-
             if (validateAccessToken($plugin, $urlToken)) {
-                echo 'valid access token in url!';
-                // redirect to homepage
+                $plugin->setAccessToken($urlToken);
+                redirectToHomepage();
             }
             else {
-                echo 'invalid access token in url!';
-                // redirect to login
+                redirectToLogin();
             }
         }
         else {
             redirectToLogin();
         }
-
-        // user doesn't have a token
-        // redirect to login page
     }
     else {
         if  (validateAccessToken($plugin, $token)) {
-            echo 'valid access token in database!';
-            // redirect to homepage
+            redirectToHomepage();
         }
         else {
-            echo 'invalid access token in database!';
-            // redirect to login
+            redirectToLogin();
         }
 
     }
 
-    $plugin->addCurrentUser();
-//    $plugin->setAccessToken('test test test');
+    testDisplayResults();
+}
 
+function testDisplayResults() {
+    global $wpdb;
     $tableName = $wpdb->prefix . 'qaUsers6';
-
-    if($wpdb->get_var("SHOW TABLES LIKE '$tableName'") == $tableName) {
-        echo $tableName . ' TABLE EXISTS!';
-    }
-    else {
-        echo '<br>TABLE DOES NOT EXIST!';
-    }
-
 
     $results = $wpdb->get_results(
         "SELECT * FROM {$tableName}"
     );
 
     echo '<br>Current Data in DB: <br>';
-
     var_dump($results);
 }
 
@@ -107,5 +104,45 @@ function validateAccessToken($plugin, $token) {
 }
 
 function redirectToHomepage() {
+    ?>
+    <div class="wrap q-and-a-plugin" id="q-and-a-plugin">
+        <?php
+        $pages = array();
+        $page_ids=get_all_page_ids();
+        foreach($page_ids as $id)
+        {
+            array_push( $pages, array($id, get_the_title($id), get_page_link($id) ) );
+        }
+        enqueue_scripts($pages);
+        readfile( 'templates.html' , __FILE__ );
+        ?>
+    </div>
+    <?php
+}
 
+function embedQuestion() {
+    $page_id = intval( $_POST['pageId'] );
+    $page = get_post( $page_id, OBJECT, 'edit' ); // get post
+    $content = $page->post_content; // get content
+    $settings =   array(
+        'media_buttons' => false, // hide media buttons
+    );
+
+    wp_editor( $content, 'embedQuestion', $settings ); // create editor
+    wp_die(); // this is required to terminate immediately and return a proper response
+}
+
+function updatePage() {
+    $post_id = intval( $_POST['pageId'] );
+
+    echo $post_id;
+
+    $updated_post = array(
+        'ID'           =>  $post_id,
+        'post_title'   => get_the_title($post_id),
+        'post_content' => $_POST['pageContent'],
+    );
+
+    wp_update_post( $updated_post ); // Update the post into the database
+    wp_die(); // this is required to terminate immediately and return a proper response
 }
